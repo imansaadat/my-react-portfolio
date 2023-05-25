08a6ffac9b03d29e51d0165bfa246f1a1d01ae0c
Iman Saadat <e.saadat72@gmail.com>
2023-05-25 22:35:05 +0330

    navbar updatde

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import Navlinks from "./Navlinks";
+import { navLinksData } from "../../data/data";
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="w-full bg-primaryColor sticky z-50 top-0 left-0 right-0 shadow-shadowOne">
+    {/* container */}
+    <div className="flex justify-between items-center py-2 max-w-screen-xl mx-auto px-3 md:px-8">
+      {/* navlinks */}
+      <ul className="hidden md:flex items-center space-x-8">
+        {navLinksData.map((item) => (
+          <Navlinks item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  </nav>
   )
 }
