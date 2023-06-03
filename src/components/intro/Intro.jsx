746fe503584ac11adda767e289dae771adf8f5e9
Iman Saadat <e.saadat72@gmail.com>
2023-06-03 21:51:32 +0330

    Intro was updated

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import ImageIntro from './ImageIntro'
+import TextIntro from './TextIntro'
 const Intro = () => {
   return (
-    <div>Intro</div>
-  )
-}
+    <section
+      id="home"
+      className="w-full grid items-center gap-12 lg:grid-cols-2 pt-10 pb-20 border-b-black border-b-[1px]"
+    >
+      <TextIntro name="iman saadat" />
+      <ImageIntro />
+    </section>
+  );
+};
 
-export default Intro
+export default Intro;
