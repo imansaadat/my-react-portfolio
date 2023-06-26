7f2620b3b1576fd9a9d87e4d84824147ecd84bd7
Iman Saadat <e.saadat72@gmail.com>
2023-06-26 22:43:24 +0330

    data was updated

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,6 @@
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe, FaBars } from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
 // NavLink Data
 export const navLinksData = [
     {
@@ -32,3 +35,42 @@ export const navLinksData = [
     },
   ];
   
+  // Features Data
+export const featuresData = [
+  {
+    id: 1,
+    icon: <FaBars />,
+    title: "Business Stratagy",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 2,
+    icon: <AiFillAppstore />,
+    title: "App Development",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 3,
+    icon: <SiProgress />,
+    title: "SEO Optimisation",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 4,
+    icon: <FaMobile />,
+    title: "Mobile Development",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 5,
+    icon: <SiAntdesign />,
+    title: "UX Design",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 6,
+    icon: <FaGlobe />,
+    title: "Hosting Websites",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+];
