135b7161e3e579f3952c9c0600a3e4454a77e404
Iman Saadat <e.saadat72@gmail.com>
2023-06-29 18:36:00 +0330

    App was updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="w-full bg-primaryColor text-light">
-       <Navbar />
+      <Navbar />
+      <main className="max-w-screen-xl mx-auto px-3 md:px-8">
+        <Intro />
+      </main>
     </div>
   );
 }
