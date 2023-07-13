ca3ecf65c263eb25e14866c0aad511372d818c82
Iman Saadat <e.saadat72@gmail.com>
2023-07-13 22:38:11 +0330

    App was updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
+import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Intro from "./components/intro/Intro";
 import Features from "./components/features/Features";
 import Projects from "./components/projects/Projects";
 import Resume from "./components/resume/Resume";
+import Contact from "./components/contact/Contact";
 import Testimonial from "./components/testimonial/Testimonial";
+import Footer from './components/footer/Footer'
 function App() {
   return (
     <div className="w-full bg-primaryColor text-light">
@@ -15,7 +17,9 @@ function App() {
         <Projects />
         <Resume />
         <Testimonial/>
+        <Contact />
       </main>
+      <Footer />
     </div>
   );
 }
