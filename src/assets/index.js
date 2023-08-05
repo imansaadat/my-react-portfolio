337ee9c2d9b1dc8571d4b960215e5de0f29d3e24
Iman Saadat <e.saadat72@gmail.com>
2023-08-06 00:06:29 +0330

    images added

---
+++ src/assets/index.js
@@ -0,0 +1,52 @@
+import logoImg from "./images/navbar/logo.png";
+import Iman from "./images/intro/iman.png";
+import keyboarda from "./images/projects/keyboarda.png";
+import epoxy from "./images/projects/epoxy.jpg";
+import sitechin from "./images/projects/sitechin.jpg";
+import landingSitechin from "./images/projects/landingSitechin.jpg";
+import defi from "./images/projects/Defi.png";
+import manageLanding from "./images/projects/manageLanding.png";
+import bookmark from "./images/projects/bookmark.png";
+import easybank from "./images/projects/easybank.jpg";
+import secured from "./images/projects/secured.png";
+import coffee from "./images/projects/coffee.jpg";
+import construction from "./images/projects/construction.jpg";
+import cars from "./images/projects/car-website-design.png";
+import food from "./images/projects/food.jpg";
+import bootcamp from "./images/projects/bootcamp.jpg";
+import delivery from "./images/projects/delivery.png";
+import weekaway from "./images/projects/weekaway.jpg";
+import clipboard from "./images/projects/clipboard.jpg";
+import dentist from "./images/projects/dentist.png";
+import portfolio from "./images/projects/portfolio.png";
+// Contact
+import contact from "./images/contact/contact.jpg";
+// Testimonial
+import user from "./images/testimonial/user.png";
+import quote from "./images/testimonial/quote.png";
+export {
+  logoImg,
+  Iman,
+  keyboarda,
+  epoxy,
+  sitechin,
+  landingSitechin,
+  defi,
+  manageLanding,
+  bookmark,
+  easybank,
+  secured,
+  coffee,
+  construction,
+  cars,
+  food,
+  bootcamp,
+  delivery,
+  portfolio,
+  weekaway,
+  clipboard,
+  dentist,
+  user,
+  quote,
+  contact,
+};
