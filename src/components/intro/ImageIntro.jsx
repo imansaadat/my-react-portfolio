28b73c00c480e339dd3f399dc5d75ff870ae9039
Iman Saadat <e.saadat72@gmail.com>
2023-08-07 13:37:40 +0330

    ImageIntro was updated

--- src/components/intro/ImageIntro.jsx
+++ src/components/intro/ImageIntro.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {eslam} from '../../assets'
+import {Iman} from '../../assets'
 const ImageIntro = () => {
   return (
     <div className='w-full relative flex items-center justify-center'>
-        <img className='w-[300px] h-[400px] md:w-[500px] md:h-[680px] z-10' src={eslam} alt='eslam saadat' />
+        <img className='w-[300px] h-[400px] md:w-[500px] md:h-[680px] z-10' src={Iman} alt='eslam saadat' />
         <div className='absolute bottom-0 w-[350px] h-[300px] md:w-[500px] md:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne'></div>
     </div>
   )
